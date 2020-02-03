43ab2b5c8210f43c2271235d7fdd10e928b03947
Sanja_K <sanja_19.96@mail.ru>
2020-02-04 00:57:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import javax.annotation.processing.Messager;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    static  int number_version = 5;
+
+    public static void main(String[] args) {
+        int border_top = 0;
+        int border_bottom = 0;
+        double step = 0;
+        double result = 0;
+        long startTime = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+	    System.out.println("Ввведите нижнию границу: ");
+        border_bottom = sc.nextInt();
+
+        System.out.println("Ввведите верхнюю границу: ");
+        border_top = sc.nextInt();
+
+        System.out.println("Ввведите шаг интегрированния: ");
+        step = sc.nextDouble();
+
+        }catch (InputMismatchException e){
+            System.out.println("Ошибка Ввода данных");
+             e.getMessage();
+        }
+
+        startTime = System.currentTimeMillis();
+
+        if ((result = computationIntegrals(border_top, border_bottom, step)) == -1){
+            System.out.println("Введены некоректные данные ");
+            return ;
+        }
+
+        System.out.println(result + " " + (System.currentTimeMillis() - startTime));
+
+    }
+
+
+    //N*√x
+    private static double computationIntegrals(double border_t, double border_b, double step){
+        double sum = 0;
+
+        if(border_b >= border_t || border_t == 0 || border_b < 0 ||
+                border_t < 0 || step <= 0 || step > (border_t-border_b) ){
+            return -1;
+        }
+
+        double resultOne = number_version*Math.sqrt( border_b);
+
+                for(double i = border_b; i<= border_t - step; i= i + step ){
+
+                    double resultTwo = number_version*Math.sqrt( (i + step));
+                    sum += ( resultOne + resultTwo )/2 *step;
+                    resultOne = resultTwo;
+                }
+        return  sum;
+    }
+}
